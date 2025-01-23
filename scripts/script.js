d527584233c5529b26bf2cf7c9d68e85c4e8b0f7
gabrielgarcianeri <19gjr98@gmail.com>
2025-01-22 19:51:29 -0800

    commit

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+console.log("Variables");
+// Keyword nameVar = value;
+
+//string
+var firstName = "Gabriel";
+var lastName = "Garcia";
+
+//numerical values
+
+let myheight=173;
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+let radius = prompt("Enter the radius: ");
+const pi = 3.1416;
+let area = pi * radius * radius
+
+let integerNumber = 375
+
+
+let userAge = prompt("Enter age: ")
+alert("Thank you very much")
+//boonlean (true/false)
+let isStudent = false
+
+let aVariable=20
+let otherVariable = 10
+console.log(aVariable,otherVariable)
+
+//display the values on the console
+console.log("My name is: " + firstName,lastName + ", I am " + myAge + " years old, my height is " + myheight);
+console.log("My name is: Gabriel Garcia, I am 21 years old, my height is 173");
+console.log("The sum is: "+ sum)
+console.log("The sum is: "+ sub)
+console.log("The sum is: "+ mul)
+console.log("The sum is: "+ div)
+console.log("The area of a circle with a radius of " + radius + " value is the " + area);
+console.log("The value of integer number is " + integerNumber)
+console.log("You are " + userAge + " years old")
