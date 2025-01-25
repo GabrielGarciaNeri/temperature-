1f79fd93ded8ed26e91eea2728ab3fa94f38a05e
gabrielgarcianeri <19gjr98@gmail.com>
2025-01-25 10:43:16 -0800

    funny cosole  story

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,20 @@
 console.log("Variables");
-// Keyword nameVar = value;
-
 //string
-var firstName = "Gabriel";
-var lastName = "Garcia";
+const friends = 3
+const totalslices = 8
+const Roboslices = 2
 
-//numerical values
+let charater = "Robo"
+let action = "eat"
+let object = "pizza"
+let slicesordered = (friends * 2)
 
-let myheight=173;
-
-let num1 = 10;
-let num2 = 2;
-
-let sum = num1 + num2;
-let sub = num1 - num2;
-let mul = num1 * num2;
-let div = num1 / num2;
-
-let radius = prompt("Enter the radius: ");
-const pi = 3.1416;
-let area = pi * radius * radius
-
-let integerNumber = 375
-
-
-let userAge = prompt("Enter age: ")
-alert("Thank you very much")
-//boonlean (true/false)
-let isStudent = false
-
-let aVariable=20
-let otherVariable = 10
-console.log(aVariable,otherVariable)
-
-//display the values on the console
-console.log("My name is: " + firstName,lastName + ", I am " + myAge + " years old, my height is " + myheight);
-console.log("My name is: Gabriel Garcia, I am 21 years old, my height is 173");
-console.log("The sum is: "+ sum)
-console.log("The sum is: "+ sub)
-console.log("The sum is: "+ mul)
-console.log("The sum is: "+ div)
-console.log("The area of a circle with a radius of " + radius + " value is the " + area);
-console.log("The value of integer number is " + integerNumber)
-console.log("You are " + userAge + " years old")
+console.log("---Funny Story---")
+console.log("There was once a robot named "+ charater + " who loved pizza but was terrible at math.")
+console.log("One day "+ charater + " was with his "+ friends + " friends and they all wanted to "+ action + " "+ object + ".")
+console.log("Each friend wanted "+ Roboslices + " slices, he also wanted "+ Roboslices + " slices.")
+console.log("So the total amount of slices "+ charater + " needed to buy were "+ totalslices + ".")
+console.log(charater + " went to order the "+ object + " but he ordered a "+ object + " with only "+ slicesordered + " slices.")
+console.log(charater + " forgot to buy " + object + " for himself.")
+console.log(charater + " remembered that he is a robot and does not need to " + action + " "+object + ".")
+console.log("So at the end of the day "+ charater + " and his "+ friends+ " friends were happy.")
