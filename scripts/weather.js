57763e01dbf234201232039b0e26a42799377b34
gabrielgarcianeri <19gjr98@gmail.com>
2025-01-27 16:44:10 -0800

    weather html css js

---
+++ scripts/weather.js
@@ -0,0 +1,8 @@
+function temperature(){
+    let num1 = prompt("Enter temperature in Celcius")
+    let Fahrenheit = (num1 * 9)/5 +32
+    console.log(`The conversion of Celcius ${num1} to Fahrenheit is ${Fahrenheit}`)
+    document.write(`The conversion of Celcius ${num1} to Fahrenheit is ${Fahrenheit}`)
+}
+
+
